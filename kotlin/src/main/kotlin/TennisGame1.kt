efba2ee3368ad9ab671ea6fcc95846c8b60f4a7e
Luís Soares <lsoares@newstore.com>
2022-09-02 14:35:15 +0100

    improve variable names

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,13 +1,13 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var player1Points: Int = 0
+    private var player2Points: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === "player1")
-            m_score1 += 1
+            player1Points += 1
         else
-            m_score2 += 1
+            player2Points += 1
     }
 
     override fun getScore(): String {
@@ -25,10 +25,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         var tempScore: Int
         for (i in 1..2) {
             if (i == 1)
-                tempScore = m_score1
+                tempScore = player1Points
             else {
                 score += "-"
-                tempScore = m_score2
+                tempScore = player2Points
             }
             when (tempScore) {
                 0 -> score += "Love"
@@ -42,7 +42,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 
     private val calculateOnAdvantageOrWin: String
         get() {
-            val minusResult = m_score1 - m_score2
+            val minusResult = player1Points - player2Points
 
             return if (minusResult == 1)
                 "Advantage player1"
@@ -54,15 +54,15 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
                 "Win for player2"
         }
 
-    private val isAdvantageOrWinScore get() = m_score1 >= 4 || m_score2 >= 4
+    private val isAdvantageOrWinScore get() = player1Points >= 4 || player2Points >= 4
 
     private val convertIfSameScore
-        get() = when (m_score1) {
+        get() = when (player1Points) {
             0    -> "Love-All"
             1    -> "Fifteen-All"
             2    -> "Thirty-All"
             else -> "Deuce"
         }
 
-    private val isSameScore get() = m_score1 == m_score2
+    private val isSameScore get() = player1Points == player2Points
 }
