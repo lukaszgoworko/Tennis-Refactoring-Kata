6ce01682f14d70c39c3f7b7969fa6f6fb39c350f
Luís Soares <lsoares@newstore.com>
2022-09-02 14:02:30 +0100

    cleanup

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -12,24 +12,24 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 
     override fun getScore(): String {
         var score = ""
-        var tempScore = 0
+        var tempScore: Int
         if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
+            score = when (m_score1) {
+                0 -> "Love-All"
+                1 -> "Fifteen-All"
+                2 -> "Thirty-All"
+                else -> "Deuce"
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
+            score = if (minusResult == 1)
+                "Advantage player1"
             else if (minusResult == -1)
-                score = "Advantage player2"
+                "Advantage player2"
             else if (minusResult >= 2)
-                score = "Win for player1"
+                "Win for player1"
             else
-                score = "Win for player2"
+                "Win for player2"
         } else {
             for (i in 1..2) {
                 if (i == 1)
--- kotlin/src/test/kotlin/TennisTest.kt
+++ kotlin/src/test/kotlin/TennisTest.kt
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import kotlin.math.max
 
+@TestInstance(PER_CLASS)
 class TennisTest {
 
     @ParameterizedTest
@@ -33,8 +37,8 @@ class TennisTest {
         checkAllScores(game, player1Score, player2Score, expectedScore)
     }
 
-    fun checkAllScores(game: TennisGame, player1Score: Int, player2Score: Int, expectedScore: String) {
-        val highestScore = Math.max(player1Score, player2Score)
+    private fun checkAllScores(game: TennisGame, player1Score: Int, player2Score: Int, expectedScore: String) {
+        val highestScore = max(player1Score, player2Score)
         for (i in 0 until highestScore) {
             if (i < player1Score)
                 game.wonPoint("player1")
@@ -44,43 +48,40 @@ class TennisTest {
         assertEquals(expectedScore, game.getScore())
     }
 
-    companion object {
-        @JvmStatic
-        fun allScores(): List<Arguments> =
-            listOf(
-                Arguments.of(0, 0, "Love-All"),
-                Arguments.of(1, 1, "Fifteen-All"),
-                Arguments.of(2, 2, "Thirty-All"),
-                Arguments.of(3, 3, "Deuce"),
-                Arguments.of(4, 4, "Deuce"),
-                Arguments.of(1, 0, "Fifteen-Love"),
-                Arguments.of(0, 1, "Love-Fifteen"),
-                Arguments.of(2, 0, "Thirty-Love"),
-                Arguments.of(0, 2, "Love-Thirty"),
-                Arguments.of(3, 0, "Forty-Love"),
-                Arguments.of(0, 3, "Love-Forty"),
-                Arguments.of(4, 0, "Win for player1"),
-                Arguments.of(0, 4, "Win for player2"),
-                Arguments.of(2, 1, "Thirty-Fifteen"),
-                Arguments.of(1, 2, "Fifteen-Thirty"),
-                Arguments.of(3, 1, "Forty-Fifteen"),
-                Arguments.of(1, 3, "Fifteen-Forty"),
-                Arguments.of(4, 1, "Win for player1"),
-                Arguments.of(1, 4, "Win for player2"),
-                Arguments.of(3, 2, "Forty-Thirty"),
-                Arguments.of(2, 3, "Thirty-Forty"),
-                Arguments.of(4, 2, "Win for player1"),
-                Arguments.of(2, 4, "Win for player2"),
-                Arguments.of(4, 3, "Advantage player1"),
-                Arguments.of(3, 4, "Advantage player2"),
-                Arguments.of(5, 4, "Advantage player1"),
-                Arguments.of(4, 5, "Advantage player2"),
-                Arguments.of(15, 14, "Advantage player1"),
-                Arguments.of(14, 15, "Advantage player2"),
-                Arguments.of(6, 4, "Win for player1"),
-                Arguments.of(4, 6, "Win for player2"),
-                Arguments.of(16, 14, "Win for player1"),
-                Arguments.of(14, 16, "Win for player2")
-            )
-    }
+    private fun allScores(): Set<Arguments> =
+        setOf(
+            Arguments.of(0, 0, "Love-All"),
+            Arguments.of(1, 1, "Fifteen-All"),
+            Arguments.of(2, 2, "Thirty-All"),
+            Arguments.of(3, 3, "Deuce"),
+            Arguments.of(4, 4, "Deuce"),
+            Arguments.of(1, 0, "Fifteen-Love"),
+            Arguments.of(0, 1, "Love-Fifteen"),
+            Arguments.of(2, 0, "Thirty-Love"),
+            Arguments.of(0, 2, "Love-Thirty"),
+            Arguments.of(3, 0, "Forty-Love"),
+            Arguments.of(0, 3, "Love-Forty"),
+            Arguments.of(4, 0, "Win for player1"),
+            Arguments.of(0, 4, "Win for player2"),
+            Arguments.of(2, 1, "Thirty-Fifteen"),
+            Arguments.of(1, 2, "Fifteen-Thirty"),
+            Arguments.of(3, 1, "Forty-Fifteen"),
+            Arguments.of(1, 3, "Fifteen-Forty"),
+            Arguments.of(4, 1, "Win for player1"),
+            Arguments.of(1, 4, "Win for player2"),
+            Arguments.of(3, 2, "Forty-Thirty"),
+            Arguments.of(2, 3, "Thirty-Forty"),
+            Arguments.of(4, 2, "Win for player1"),
+            Arguments.of(2, 4, "Win for player2"),
+            Arguments.of(4, 3, "Advantage player1"),
+            Arguments.of(3, 4, "Advantage player2"),
+            Arguments.of(5, 4, "Advantage player1"),
+            Arguments.of(4, 5, "Advantage player2"),
+            Arguments.of(15, 14, "Advantage player1"),
+            Arguments.of(14, 15, "Advantage player2"),
+            Arguments.of(6, 4, "Win for player1"),
+            Arguments.of(4, 6, "Win for player2"),
+            Arguments.of(16, 14, "Win for player1"),
+            Arguments.of(14, 16, "Win for player2")
+        )
 }
